3f873e106a428e0d3a7a8c84a934696f2c579e53
Dongkyoo <wind.dong.dream@gmail.com>
2019-11-01 00:38:14 +0900

    채팅 레이아웃 구현

--- app/src/main/java/com/dongkyoo/gongzza/chattingRoomList/ChattingRoomListFragment.java
+++ app/src/main/java/com/dongkyoo/gongzza/chattingRoomList/ChattingRoomListFragment.java
@@ -1,13 +1,16 @@
 package com.dongkyoo.gongzza.chattingRoomList;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.dongkyoo.gongzza.R;
+import com.dongkyoo.gongzza.chattingRoom.ChatActivity;
 
 /**
  * 작성자: 이동규
@@ -23,7 +26,21 @@ public class ChattingRoomListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chatting_room_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_chatting_room_list, container, false);
+        initView(view);
+
+        return view;
+    }
+
+    private void initView(View view) {
+        RelativeLayout chattingList = view.findViewById(R.id.chattingRoom);
+        chattingList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
