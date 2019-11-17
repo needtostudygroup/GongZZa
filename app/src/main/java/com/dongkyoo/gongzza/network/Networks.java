a1dcc01bfbd4ac667c1f3bfb6d3ebe86ebcdaa8b
Dongkyoo <wind.dong.dream@gmail.com>
2019-11-18 00:00:59 +0900

    서버 주소 변경

--- app/src/main/java/com/dongkyoo/gongzza/network/Networks.java
+++ app/src/main/java/com/dongkyoo/gongzza/network/Networks.java
@@ -1,15 +1,14 @@
 package com.dongkyoo.gongzza.network;
 
-import com.google.gson.GsonBuilder;
-
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class Networks {
 
+    public static final String SERVER_URL = "http://114.206.137.114:8080";
+
     public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://114.206.137.114:8080")
+            .baseUrl(SERVER_URL)
             .addConverterFactory(JacksonConverterFactory.create())
             .build();
 }
