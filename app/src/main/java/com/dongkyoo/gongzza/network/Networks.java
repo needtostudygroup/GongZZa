d81f5507be37fdbdf09f05411bc543bf42f95d14
Dongkyoo <wind.dong.dream@gmail.com>
2019-11-18 00:32:32 +0900

    서버 주소 개인화

--- app/src/main/java/com/dongkyoo/gongzza/network/Networks.java
+++ app/src/main/java/com/dongkyoo/gongzza/network/Networks.java
@@ -1,11 +1,29 @@
 package com.dongkyoo.gongzza.network;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
 public class Networks {
 
-    public static final String SERVER_URL = "http://114.206.137.114:8080";
+    private static String SERVER_URL;
+
+    static {
+        try {
+            File file = new File("src/main/assets/server.properties");
+            System.out.println(file.getAbsolutePath());
+            InputStream is = new FileInputStream("src/main/assets/server.properties");
+            Properties properties = new Properties();
+            properties.load(is);
+            SERVER_URL = properties.getProperty("serverUrl");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(SERVER_URL)
