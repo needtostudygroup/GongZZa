16338a89523cb2bee555322fb747b04421dca262
JungHyun <f7817455@naver.com>
2019-11-23 23:15:33 +0900

    회원가입 서버 통신 구현 테스트 및 생년월일 예외처리 추가

--- app/src/main/java/com/dongkyoo/gongzza/Login/SignupActivity.java
+++ app/src/main/java/com/dongkyoo/gongzza/Login/SignupActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.InputFilter;
 import android.text.Spanned;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -17,21 +18,54 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.dongkyoo.gongzza.MockData;
 import com.dongkyoo.gongzza.R;
+import com.dongkyoo.gongzza.network.Networks;
+import com.dongkyoo.gongzza.network.UserApi;
+import com.dongkyoo.gongzza.vos.User;
 
 import org.w3c.dom.Text;
 
+import java.sql.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Tag;
+
 public class SignupActivity extends AppCompatActivity {
 
+    UserApi userApi = Networks.retrofit.create(UserApi.class);
+    Call<User> call = userApi.signUp(MockData.getMockUser());
+
     DatePicker Birth;
+    private  String birthString;
+    private Date birthDate;
+    private  boolean isChanged = false;
+    private  boolean isComplete = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_signup);
+
+        //객체성성
+        User user = new User();
+
+        Birth = (DatePicker)findViewById(R.id.inputBirth);
+        //현재날짜로 초기화
+        Birth.init(Birth.getYear(), Birth.getMonth(), Birth.getDayOfMonth(), new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                birthString = String.format("%d-%d-%d", Birth.getYear(), Birth.getMonth() + 1, Birth.getDayOfMonth());
+                birthDate = Date.valueOf(birthString);
+                isChanged = true;
+            }
+        });
 
         findViewById(R.id.emailConfirmButton).setOnClickListener(new Button.OnClickListener() {
             @Override
@@ -45,11 +79,13 @@ public class SignupActivity extends AppCompatActivity {
                 alertDialog.show();
             }
         });
+
         findViewById(R.id.signupedButton).setOnClickListener(new View.OnClickListener(){
             EditText signupEtName = (EditText)findViewById(R.id.inputName);
             EditText signupEtEmail = (EditText)findViewById((R.id.inputEmailCheck));
             EditText signupEtPassword = (EditText)findViewById((R.id.inputPassword));
             EditText signupEtPasswordConfirm = (EditText)findViewById((R.id.inputPasswordConfirm));
+            EditText signupEtId = (EditText)findViewById(R.id.inputId);
 
             @Override
             public void onClick(View view) {
@@ -58,12 +94,26 @@ public class SignupActivity extends AppCompatActivity {
                     signupEtName.requestFocus();
                     return;
                 }
-                else if(signupEtEmail.getText().toString().length() == 0){
+                else
+                    user.setName(signupEtName.getText().toString());
+
+                if(signupEtEmail.getText().toString().length() == 0){
                     Toast.makeText(SignupActivity.this, "Email을 입력하세요",Toast.LENGTH_SHORT).show();
                     signupEtEmail.requestFocus();
                     return;
                 }
-                else if(signupEtPassword.getText().toString().length() == 0){
+                else
+                    user.setEmail(signupEtEmail.getText().toString());
+
+                if(signupEtId.getText().toString().length() == 0) {
+                    Toast.makeText(SignupActivity.this, "ID를 입력하세요", Toast.LENGTH_SHORT).show();
+                    signupEtId.requestFocus();
+                    return;
+                }
+                else
+                    user.setId(signupEtId.getText().toString());
+
+                if(signupEtPassword.getText().toString().length() == 0){
                     Toast.makeText(SignupActivity.this, "Password를 입력하세요",Toast.LENGTH_SHORT).show();
                     signupEtPassword.requestFocus();
                     return;
@@ -79,20 +129,40 @@ public class SignupActivity extends AppCompatActivity {
                     signupEtPasswordConfirm.requestFocus();
                     return;
                 }
-                else{
-                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(intent);
-                }
-            }
-        });
+                else
+                    user.setPassword(signupEtPasswordConfirm.getText().toString());
 
-        Birth = (DatePicker)findViewById(R.id.inputBirth);
-        //현재날짜로 초기화
-        Birth.init(Birth.getYear(), Birth.getMonth(), Birth.getDayOfMonth(), new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                String res = String.format("%d년 %d월 %d일", Birth.getYear() +1, Birth.getMonth(), Birth.getDayOfMonth());
-                Toast.makeText(SignupActivity.this,res,Toast.LENGTH_SHORT).show();
+                if (isChanged){
+                    Toast.makeText(SignupActivity.this,birthString,Toast.LENGTH_SHORT).show();
+                    user.setBirthday(birthDate);
+                }
+                else
+                    Toast.makeText(SignupActivity.this, "생년월일을 입력해 주세요.",Toast.LENGTH_SHORT).show();
+
+                if(user.getId() != null){
+                    //모든 데이터 입력 했을 시
+                    //서버에 데이터 전송
+                    call.enqueue(new Callback<User>() {
+                        @Override
+                        public void onResponse(Call<User> call, Response<User> response) {
+                            //@POST("/users")
+                            //Call<User> signUp(@Body User user);
+                            if (response.code() == 200) {
+                                Toast.makeText(SignupActivity.this, "회원가입이 완료되었습니다.",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(SignupActivity.this, "통신은 성공했으나 회원가입에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        @Override
+                        public void onFailure(Call<User> call, Throwable t) {
+                            Toast.makeText(SignupActivity.this, "서버와 통신에 실패하였습니다. 다시 시도해 주세요.",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
             }
         });
     }
