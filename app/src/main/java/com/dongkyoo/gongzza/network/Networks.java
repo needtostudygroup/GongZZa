d87fb0b75addef1b1155d45cf28d548591697d93
Dongkyoo <wind.dong.dream@gmail.com>
2019-12-20 19:15:53 +0900

    최종

--- app/src/main/java/com/dongkyoo/gongzza/network/Networks.java
+++ app/src/main/java/com/dongkyoo/gongzza/network/Networks.java
@@ -9,7 +9,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 public class Networks {
 
     public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://192.168.1.38:1234")
+            .baseUrl("http://n09app.cafe24.com")
             .addConverterFactory(JacksonConverterFactory.create())
             .build();
 }
